c3844acebe6df6fb684e6819de7bd2344d2bcc85
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-04-21 08:10:11 +0100

    first commit

---
+++ 02-introducing-jsx/01_JSX_Basics/index.js
@@ -0,0 +1,15 @@
+
+
+class JSXDemo extends React.Component {
+	render () {
+		return (
+			<div>
+				<h1>My Image!</h1>
+				<img src="" />
+			</div>
+		);
+	}
+}
+
+
+ReactDOM.render(<JSXDemo/>, document.getElementById('root'));
---
+++ 02-introducing-jsx/03_JSX_Conditionals/index.js
@@ -0,0 +1,31 @@
+
+function getNum() {
+	return Math.floor(Math.random() * 10) + 1;
+}
+
+class NumPicker extends React.Component {
+	render() {
+		const num = getNum();
+
+		let msg;
+		if (num === 7) {
+			msg =
+				<div>
+					<h2>CONGRATS YOU WIN!</h2>
+					<img src="" alt="this is an image"></img>
+				</div>
+		} else {
+			msg = <p>Sorry You Lose!</p>;
+		}
+
+		return (
+			<div>
+				<h1>Your number is {num}</h1>
+				{msg}
+			</div>
+		);
+	}
+}
+
+
+ReactDOM.render(<NumPicker />, document.getElementById('root'));
