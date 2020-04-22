30334c22fefe2d12fb6b4cf7ec6bcbb31d85519a
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-04-22 07:33:11 +0100

    EXERCISE: Slot Machine

--- 03-props-and-more/03_Slots_Exercise/Machine.js
+++ 03-props-and-more/03_Slots_Exercise/Machine.js
@@ -1,14 +1,29 @@
 class Machine extends React.Component {
 
     render() {
-        if(this.props.s1 === this.props.s2){
-            console.log("You Win");
+
+        let msg;
+        const s1 = this.props.s1;
+        const s2 = this.props.s2;
+        const s3 = this.props.s3;
+
+        if (s1 === s2 && s1 === s3) {
+            msg =
+                <div>
+                    <p>You Win!</p>
+                </div>
+        } else {
+            msg =
+                <div>
+                    <p>You Lose!</p>
+                </div>
         }
         return (
             <div>
-                <p>{this.props.s1}</p>
-                <p>{this.props.s2}</p>
-                <p>{this.props.s3}</p>
+                <p>{s1}</p>
+                <p>{s2}</p>
+                <p>{s3}</p>
+                <div>{msg}</div>
             </div>
         );
     }
