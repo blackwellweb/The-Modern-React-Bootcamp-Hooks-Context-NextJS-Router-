feccf651a04bb67905a7789afe69935dbc59c05f
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-04-24 07:58:47 +0100

    EXERICSE: Fruits Modules (100%)

--- 04-create-react-app/exercise-fruits-modules/src/index.js
+++ 04-create-react-app/exercise-fruits-modules/src/index.js
@@ -2,9 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import fruits from './foods';
 
-console.log(fruits);
+import fruits from './foods';
+import { choice, remove } from './helpers';
+
+
+const randomFruit = choice(fruits);
+console.log(`I’d like one ${randomFruit}, please.`);
+console.log(`Here you go: ${randomFruit}`)
+console.log('Delicious! May I have another?');
+
+remove(fruits,randomFruit);
+console.log(`I’m sorry, we’re all out. We have ${fruits.length} left.`)
+
 
 class App extends React.Component {
     render(){
