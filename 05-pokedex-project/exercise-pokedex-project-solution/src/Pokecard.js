fbed7d97b97dfe9e87aca39016d5da7ac12b457c
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-04-29 08:10:22 +0100

    Adding Fancier Images

--- 05-pokedex-project/exercise-pokedex-project-solution/src/Pokecard.js
+++ 05-pokedex-project/exercise-pokedex-project-solution/src/Pokecard.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
-const Poke_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const Poke_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const Poke_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+// This function takes a number, if the number s is greater 
+// than 999 then don’t pad just return number, else take to zeros plus the number then slice 3 digits out of that and return those three 
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
     render() {
-        let imgSrc = `${Poke_API}${this.props.id}.png`
+        let imgSrc = `${Poke_API}${padToThree(this.props.id)}.png`
         return (
             <div className="Pokecard">
                 <h1 className="Pokecard-title">{this.props.name}</h1>
