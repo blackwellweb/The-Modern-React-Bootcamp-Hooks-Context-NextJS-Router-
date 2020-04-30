d239a7c760df45a61db05c857804c3fe50b044fb
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-04-30 09:13:14 +0100

    Initializing State

---
+++ 06-react-state/introducting-state/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Game from './Game';
+import Game2 from './Game2';
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+      <Game2 />
+    </div>
+  );
+}
+
+export default App;
