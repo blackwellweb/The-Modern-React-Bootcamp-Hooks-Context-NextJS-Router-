2b00db05f7e61397f419c638d11f8347e84c1458
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-01 08:30:08 +0100

    Setting State Correctly

--- 06-react-state/introducting-state/src/App.js
+++ 06-react-state/introducting-state/src/App.js
@@ -4,13 +4,12 @@ import './App.css';
 import Game from './Game';
 import Game2 from './Game2';
 import Demo from './Demo';
+import Rando from './Rando';
 
 function App() {
   return (
     <div className="App">
-      <Game />
-      <Game2 />
-      <Demo animal="Bobcat" food="Pineapple" />
+      <Rando maxNum={7} />
     </div>
   );
 }
