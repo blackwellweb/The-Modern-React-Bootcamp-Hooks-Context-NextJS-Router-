4a07eec54fd1e4667b69834e74a46045e098af2a
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-01 08:45:14 +0100

    Crash Course: Click Events in React (40%)

--- 06-react-state/introducting-state/src/App.js
+++ 06-react-state/introducting-state/src/App.js
@@ -5,11 +5,13 @@ import Game from './Game';
 import Game2 from './Game2';
 import Demo from './Demo';
 import Rando from './Rando';
+import Button from './Button';
+import BrokenClick from './BrokenClick';
 
 function App() {
   return (
     <div className="App">
-      <Rando maxNum={7} />
+      <BrokenClick />
     </div>
   );
 }
---
+++ 06-react-state/introducting-state/src/BrokenClick.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class BrokenClick extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {clicked: false}
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.clicked ? 'Clicked!!!' : 'Not Clicked'}</h1>
+                <button>Click Me!</button>
+            </div>
+        )
+    }
+}
+
+export default BrokenClick;
