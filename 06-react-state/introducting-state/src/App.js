32ffaeb6ef2c9ff92e0603ea980e4a38cabd1963
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-01 08:04:42 +0100

    WTF is super() Vs. super(props)

--- 06-react-state/introducting-state/src/App.js
+++ 06-react-state/introducting-state/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Game from './Game';
 import Game2 from './Game2';
+import Demo from './Demo';
 
 function App() {
   return (
     <div className="App">
       <Game />
       <Game2 />
+      <Demo animal="Bobcat" food="Pineapple" />
     </div>
   );
 }
