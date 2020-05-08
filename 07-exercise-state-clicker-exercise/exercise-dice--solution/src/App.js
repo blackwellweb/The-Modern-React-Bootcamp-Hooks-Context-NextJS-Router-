f36546a5d90cdf877868c19369163b5b00050677
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-08 08:29:04 +0100

    Writing the Die Component

---
+++ 07-exercise-state-clicker-exercise/exercise-dice--solution/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Die from './Die';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Die face="five"/>
+    </div>
+  );
+}
+
+export default App;
