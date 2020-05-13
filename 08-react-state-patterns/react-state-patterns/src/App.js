e3b5bc256aa59fcbd5686c9d757b972711fa5127
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-13 08:06:43 +0100

    Updating Existing State

---
+++ 08-react-state-patterns/react-state-patterns/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import ScoreKeeper from './ScoreKeeper';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ScoreKeeper />
+    </div>
+  );
+}
+
+export default App;
