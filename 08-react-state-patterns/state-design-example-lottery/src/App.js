25c5c5926292009507dc8fba28af95c8297a9267
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-14 07:50:36 +0100

    State Design Example: LottoBall Component

---
+++ 08-react-state-patterns/state-design-example-lottery/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Ball from './Ball';
+
+function App() {
+  return (
+    <div className="App">
+      <Ball num={17}/>
+      <Ball num={15}/>
+    </div>
+  );
+}
+
+export default App;
