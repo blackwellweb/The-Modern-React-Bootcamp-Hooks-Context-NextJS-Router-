79189e9dfdce51947c5ef157e017d2f2fcdb38ba
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-14 08:42:24 +0100

    State Exercise 1: Coin Flipper (10%)

---
+++ 09-state-exercises/coin-flipper/src/Coin.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Coin extends Component {
+    
+    render(){
+        return(
+            <div className="Coin">
+                <h1>I am a coin</h1>
+            </div>
+        );
+    }
+    
+}
+
+export default Coin;
