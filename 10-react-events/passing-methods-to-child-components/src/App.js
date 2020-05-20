58c5eb33daaa4801931021acbec4f4311d7f57d8
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-20 08:32:12 +0100

    Passing Methods to Child Components

--- 10-react-events/passing-methods-to-child-components/src/App.js
+++ 10-react-events/passing-methods-to-child-components/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import NumberList from './NumberList';
+import BetterNumberList from './BetterNumberList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <NumberList />
+      <BetterNumberList />
     </div>
   );
 }
