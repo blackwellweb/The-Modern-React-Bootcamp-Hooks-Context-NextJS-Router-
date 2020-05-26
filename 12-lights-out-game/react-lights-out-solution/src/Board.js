c3a10e7eaed0de709a015a154c6ceb518218e600
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-26 07:49:18 +0100

    Flipping Cells

--- 12-lights-out-game/react-lights-out-solution/src/Board.js
+++ 12-lights-out-game/react-lights-out-solution/src/Board.js
@@ -79,13 +79,23 @@ class Board extends Component {
       }
     }
 
+  
+  
     // TODO: flip this cell and the cells around it
+    flipCell(y, x);   // Flip initial cell
+    flipCell(y, x -1); // flip left
+    flipCell(y, x + 1); // flip right
+    flipCell(y -1, x); // flip below
+    flipCell(y + 1, x); // flip above
+
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
+    let hasWon = false;
 
-    //this.setState({board, hasWon});
+    this.setState({board, hasWon});
   }
+
 
 
   /** Render game board or winning message. */
@@ -103,8 +113,8 @@ class Board extends Component {
     for (let y = 0; y < this.props.nrows; y++) {
       let row = []
       for (let x = 0; x < this.props.ncols; x++) {
-      let coord = `${y}-${x}`;
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]} />)
+        let coord = `${y}-${x}`;
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} flipCellsAroundMe={() => this.flipCellsAround(coord)} />)
       }
       tblBoard.push(<tr key={y} >{row}</tr>)
     }
@@ -112,9 +122,7 @@ class Board extends Component {
     return (
       <table className="Board">
         <tbody>
-          <tr>
-            {tblBoard}
-          </tr>
+          {tblBoard}
         </tbody>
       </table>
     )
