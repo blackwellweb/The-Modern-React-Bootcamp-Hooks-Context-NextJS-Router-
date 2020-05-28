6ec5ddaf979da65f26bfab3b1c8c77c8179e2a73
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-28 08:11:32 +0100

    Using the UUID Library

--- 13-forms-in-react/passing-data-upwards/src/ShoppingList.js
+++ 13-forms-in-react/passing-data-upwards/src/ShoppingList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ShoppingListForm from './ShoppingListForm'
+import { v4 as uuidv4 } from 'uuid';
 
 
 class ShoppingList extends Component {
@@ -7,18 +8,21 @@ class ShoppingList extends Component {
         super(props);
         this.state = {
             items: [
-                { name: 'Milk', qty: '2 gallons' },
-                { name: 'Bread', qty: '2 loaves' },
+                { name: 'Milk', qty: '2 gallons', id: uuidv4() },
+                { name: 'Bread', qty: '2 loaves', id: uuidv4() },
             ]
         };
         this.addItem = this.addItem.bind(this);
     };
 
 
-    addItem(item){
+    addItem(item) {
+
+        let newItem = { ...item, id: uuidv4() }
+
         // Take all the old items, add in our new item and put it in a new array and set that to be the value of items
         this.setState(state => ({
-            items: [...state.items,item]
+            items: [...state.items, newItem]
         }))
     }
 
@@ -26,7 +30,7 @@ class ShoppingList extends Component {
         return (
             <ul>
                 {this.state.items.map(item => (
-                    <li>
+                    <li key={item.id}>
                         {item.name}:{item.qty}
                     </li>
                 ))}
@@ -36,11 +40,11 @@ class ShoppingList extends Component {
 
 
     render() {
-        return(
+        return (
             <div>
                 <h1>ShoppingList</h1>
                 {this.renderItems()}
-                <ShoppingListForm addItem={this.addItem}/>
+                <ShoppingListForm addItem={this.addItem} />
             </div>
         )
     }
