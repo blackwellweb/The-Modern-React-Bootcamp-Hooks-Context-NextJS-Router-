4c9c067bbc6f7c7133164306d975b5f69326030f
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-28 08:01:08 +0100

    Design Pattern: Passing Data Upwards

--- 13-forms-in-react/passing-data-upwards/src/ShoppingList.js
+++ 13-forms-in-react/passing-data-upwards/src/ShoppingList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ShoppingListForm from './ShoppingListForm'
 
 
 class ShoppingList extends Component {
@@ -9,9 +10,17 @@ class ShoppingList extends Component {
                 { name: 'Milk', qty: '2 gallons' },
                 { name: 'Bread', qty: '2 loaves' },
             ]
-        }
+        };
+        this.addItem = this.addItem.bind(this);
     };
 
+
+    addItem(item){
+        // Take all the old items, add in our new item and put it in a new array and set that to be the value of items
+        this.setState(state => ({
+            items: [...state.items,item]
+        }))
+    }
 
     renderItems() {
         return (
@@ -31,6 +40,7 @@ class ShoppingList extends Component {
             <div>
                 <h1>ShoppingList</h1>
                 {this.renderItems()}
+                <ShoppingListForm addItem={this.addItem}/>
             </div>
         )
     }
