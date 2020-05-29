a7dd6e446a2d6cec287f1c606a78841518e1e2c2
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-29 08:18:40 +0100

    handleSubmit add to NewBoxForm.js

--- 14-forms-exercise/color-box-maker/src/NewBoxForm.js
+++ 14-forms-exercise/color-box-maker/src/NewBoxForm.js
@@ -7,6 +7,17 @@ class NewBoxForm extends Component {
         super(props);
         this.state = { height: '', width: '' }
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+
+        // This will pass the state (Height and width) up to the parent component BoxList.js and it will use it 
+        this.props.addBox(this.state);
+
+        this.state = { height: '', width: '' }
     }
 
     handleChange(evt) {
@@ -17,24 +28,25 @@ class NewBoxForm extends Component {
 
 
     render() {
-        return(
-            <form className="NewBoxForm">
-            <h1>Make a box</h1>
-            <label htmlFor="height">Box height:</label>
-            <input
-                id="height"
-                name="height"
-                value={this.state.height}
-                onChange={this.handleChange}
-            />
-            <label htmlFor="width">Box height:</label>
-            <input
-                id="width"
-                name="width"
-                value={this.state.width}
-                onChange={this.handleChange}
-            />
-        </form>
+        return (
+            <form className="NewBoxForm" onSubmit={this.handleSubmit}>
+                <h1>Make a box</h1>
+                <label htmlFor="height">Box height:</label>
+                <input
+                    id="height"
+                    name="height"
+                    value={this.state.height}
+                    onChange={this.handleChange}
+                />
+                <label htmlFor="width">Box height:</label>
+                <input
+                    id="width"
+                    name="width"
+                    value={this.state.width}
+                    onChange={this.handleChange}
+                />
+                <button>Make a new box</button>
+            </form>
         );
     }
 
