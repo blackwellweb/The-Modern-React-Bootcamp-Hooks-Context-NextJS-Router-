bb0cb35c4ac475ea9e55e4ed82bec49928896e36
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-29 07:47:21 +0100

    NewBoxForm added

--- 14-forms-exercise/color-box-maker/src/BoxList.js
+++ 14-forms-exercise/color-box-maker/src/BoxList.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Box from './Box'
+import NewBoxForm from './NewBoxForm';
+import Box from './Box';
 
 class BoxList extends Component {
 
     render(){
         return (
             <div className='BoxList'>
-                <h1>I am the BoxList</h1>
+                <NewBoxForm />
             </div>
         )
     }
--- 14-forms-exercise/color-box-maker/src/NewBoxForm.js
+++ 14-forms-exercise/color-box-maker/src/NewBoxForm.js
@@ -1,1 +1,43 @@
 import React, { Component } from 'react';
+
+
+class NewBoxForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { height: '', width: '' }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+
+    render() {
+        return(
+            <form className="NewBoxForm">
+            <h1>Make a box</h1>
+            <label htmlFor="height">Box height:</label>
+            <input
+                id="height"
+                name="height"
+                value={this.state.height}
+                onChange={this.handleChange}
+            />
+            <label htmlFor="width">Box height:</label>
+            <input
+                id="width"
+                name="width"
+                value={this.state.width}
+                onChange={this.handleChange}
+            />
+        </form>
+        );
+    }
+
+}
+
+export default NewBoxForm;
