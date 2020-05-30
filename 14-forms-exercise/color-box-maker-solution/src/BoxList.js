c287911bafc0bd10fc129c7ef3d96e73163c3223
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-05-30 17:56:32 +0100

    Creating the BoxForm Component

--- 14-forms-exercise/color-box-maker-solution/src/BoxList.js
+++ 14-forms-exercise/color-box-maker-solution/src/BoxList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Box from './Box';
+import NewBoxForm from './NewBoxForm';
 
 class BoxList extends Component {
 
@@ -7,8 +8,15 @@ class BoxList extends Component {
     constructor(props) {
         super(props);
         this.state = { boxes: [{ width: 10, height: 40, color: 'orange' }] };
+        this.create = this.create.bind(this);
     }
 
+
+    create(newBox){
+        this.setState({
+            boxes: [...this.state.boxes,newBox]
+        });
+    }
 
     render() {
         const boxes = this.state.boxes.map(box => 
@@ -22,6 +30,7 @@ class BoxList extends Component {
         return (
             <div>
                 <h1>Color Box Maker Thingy</h1>
+                <NewBoxForm createBox={this.create}/>
                 {boxes}
             </div>
         );
