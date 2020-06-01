8b5809708c5040371ecc05aff3b1795b0f369fde
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-06-01 08:12:36 +0100

    Todo built

---
+++ 15-todo-app/todo-app/src/Todo.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Todo.css';
+
+class Todo extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+    }
+
+
+    handleEdit(evt){
+        // This will run a method passed in as a prop by the parent component 
+        console.log('I was edited');
+    }
+
+    handleRemove(evt){
+        // This will run a method passed in as a prop by the parent component 
+        console.log('I was removed');
+    }
+
+    render(){
+        return(
+            <div className="Todo">
+                <span>{this.props.task}</span>
+                <i onClick={this.handleEdit} className="fas fa-pen Todo__edit"></i>
+                <i onClick={this.handleRemove} className="fas fa-dumpster Todo__remove"></i>
+            </div>
+        );
+    }
+
+
+}
+
+
+export default Todo;
