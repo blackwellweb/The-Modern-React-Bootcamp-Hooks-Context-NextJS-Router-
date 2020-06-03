18535975eaabbe485f0268770497de88a26a958f
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-06-03 08:07:26 +0100

    Can no edit todo's

--- 15-todo-app/todo-app/src/Todo.js
+++ 15-todo-app/todo-app/src/Todo.js
@@ -6,11 +6,25 @@ class Todo extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { showForm: false }
+        this.state = { showForm: false , text: this.props.text, edited: ''}
 
 
         this.handleEdit = this.handleEdit.bind(this);
         this.handleRemove = this.handleRemove.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleChange(evt) {
+        this.setState({edited: evt.target.value});
+    }
+
+
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+        this.setState({showForm: false , text: this.state.edited, edited: ''});
     }
 
 
@@ -28,14 +42,14 @@ class Todo extends Component {
 
         if (this.state.showForm) {
             content =
-                <form className="Todo__form">
-                    <input type="text" placeholder={this.state.text} />
+                <form onSubmit={this.handleSubmit} className="Todo__form">
+                    <input type="text" placeholder={this.state.text}  onChange={this.handleChange} />
                     <button>Save</button>
                 </form>
         } else {
             content =
                 <div>
-                    <span>{this.props.text}</span>
+                    <span>{this.state.text}</span>
                     <i onClick={this.handleEdit} className="fas fa-pen Todo__edit"></i>
                     <i onClick={this.handleRemove} className="fas fa-dumpster Todo__remove"></i>
                 </div>
