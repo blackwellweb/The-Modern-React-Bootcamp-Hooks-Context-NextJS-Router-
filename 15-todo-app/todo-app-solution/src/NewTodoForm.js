9cb6edcec223a95b232a80c351903769f81585cb
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-06-08 07:52:22 +0100

    Styling the Todo App(100%)

--- 15-todo-app/todo-app-solution/src/NewTodoForm.js
+++ 15-todo-app/todo-app-solution/src/NewTodoForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { v4 as uuid } from 'uuid';
+import './NewTodoFrom.css';
 
 class NewTodoForm extends Component {
 
@@ -24,7 +25,7 @@ class NewTodoForm extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form className="NewTodoForm" onSubmit={this.handleSubmit}>
                 <label htmlFor="task">New Todo</label>
                 <input
                     type="text"
