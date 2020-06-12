3fc415cdb6970aec2bd1bcfc0041b93e5def6ce6
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-06-12 08:01:04 +0100

    Animating Dice Rolls

--- 16-building-yahztee/yahtzee-starter/src/Die.js
+++ 16-building-yahztee/yahtzee-starter/src/Die.js
@@ -3,8 +3,10 @@ import "./Die.css";
 
 class Die extends Component {
 
+  // val: 5 in the defaultProps will just allow the Die to animate when the page first loads and will be immediately overridden after
   static defaultProps = {
-    numberWords: ['one', 'two', 'three', 'four', 'five', 'six']
+    numberWords: ['one', 'two', 'three', 'four', 'five', 'six'],
+    val: 5
   };
 
   constructor(props) {
@@ -18,10 +20,11 @@ class Die extends Component {
 
 
   render() {
-    const {numberWords, locked, val, disabled} = this.props;
+    const {numberWords, locked, val, disabled, rolling} = this.props;
 
     let classes = ` Die fas fa-dice-${numberWords[val - 1]} fa-5x `
     if(locked) classes += 'Die-locked';
+    if(rolling) classes += 'Die-rolling';
 
     return <i className={classes} onClick={this.handleClick} disabled={disabled}></i>
  
--- 16-building-yahztee/yahtzee-starter/src/Game.js
+++ 16-building-yahztee/yahtzee-starter/src/Game.js
@@ -13,6 +13,7 @@ class Game extends Component {
       dice: Array.from({ length: NUM_DICE }),
       locked: Array(NUM_DICE).fill(false),
       rollsLeft: NUM_ROLLS,
+      rolling: false,
       scores: {
         ones: undefined,
         twos: undefined,
@@ -32,7 +33,21 @@ class Game extends Component {
     this.roll = this.roll.bind(this);
     this.doScore = this.doScore.bind(this);
     this.toggleLocked = this.toggleLocked.bind(this);
+    this.animateRoll = this.animateRoll.bind(this);
   }
+
+  // When page first loads
+  componentDidMount() {
+    this.animateRoll();
+  }
+
+
+  animateRoll() {
+    this.setState({rolling: true}, () => {
+      setTimeout(this.roll, 1000);
+    });
+  }
+
 
   roll(evt) {
 
@@ -42,14 +57,15 @@ class Game extends Component {
         st.locked[i] ? d : Math.ceil(Math.random() * 6)
       ),
       locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
-      rollsLeft: st.rollsLeft - 1
+      rollsLeft: st.rollsLeft - 1,
+      rolling: false
     }));
   }
 
   toggleLocked(idx) {
 
     // check if the user can still roll before toggling the Die
-    if (this.state.rollsLeft > 0) {
+    if (this.state.rollsLeft > 0 && !this.state.rolling) {
       // toggle whether idx is in locked or not
       this.setState(st => ({
         locked: [
@@ -85,12 +101,13 @@ class Game extends Component {
               locked={this.state.locked}
               handleClick={this.toggleLocked}
               disabled={this.state.rollsLeft === 0}
+              rolling={this.state.rolling}
             />
             <div className='Game-button-wrapper'>
               <button
                 className='Game-reroll'
                 disabled={this.state.locked.every(x => x) || this.state.rollsLeft === 0}
-                onClick={this.roll}
+                onClick={this.animateRoll}
               >
                 {this.state.rollsLeft} Rerolls Left
               </button>
