c756d5ab1f17f7ac42ca0dee81a20f23c7e4e13a
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-06-15 08:06:38 +0100

    Introducing ComponentDidMount

---
+++ 17-lifecycle-methods/component-did-mount/src/Timer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { time: new Date() };
+
+        console.log('In constructor');
+    }
+
+    componentDidMount() {
+        console.log('In component did mount');
+        this.timerID = setInterval(() => {
+            this.setState({ time: new Date });
+        }, 1000);
+    }
+
+
+
+    render() {
+        console.log('in render');
+        return <h1>{this.state.time.getSeconds()}</h1>
+    }
+}
+
+export default Timer;
