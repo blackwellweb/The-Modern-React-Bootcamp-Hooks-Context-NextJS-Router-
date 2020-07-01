4e7ab4ef8e96714d7396653331994f8de7480df1
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-01 21:59:26 +0100

    Added second API call to get card images

--- 18-lifecycle-exercise/cards-api-project/src/Deck.js
+++ 18-lifecycle-exercise/cards-api-project/src/Deck.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { v4 as uuid } from 'uuid';
 import Card from './Card';
 
 const API_URL = "https://deckofcardsapi.com/api/deck/new/shuffle";
@@ -8,7 +9,7 @@ class Deck extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { deckID: '', cards: '' };
+        this.state = { deckID: null, cards: [] };
 
         this.handleClick = this.handleClick.bind(this);
     }
@@ -17,38 +18,56 @@ class Deck extends Component {
         // Get deck ID
         axios.get(API_URL).then(response => {
             this.setState({ deckID: response.data.deck_id });
-
         });
 
-
-        // const id = this.state.deckID;
-        //     axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/`).then(response => {
-        //         // this.setState({ deckID: response.data.deck_id });
-        //         console.log(response.data)
-        //     });
     }
 
 
+    async getNewCard() {
+        const id = this.state.deckID;
+        axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/`).then(response => {
+            const newCard = response.data.cards[0].image;
 
+            this.setState(curState => {
+                return {
+                    cards: curState.cards.concat(newCard)
+                }
+            })
 
+            // Do the same as the code above 
+            // this.setState({
+            //     cards: [...this.state.cards, newCard]
+            // })
 
-    makeNewCard() {
-        return (
-            <Card dID={this.state.deckID} />
-        )
-
+        });
     }
+
+
 
     handleClick(evt) {
-        console.log('I was clicked')
-
+        this.getNewCard();
     }
 
 
     render() {
+
+        // this.state.cards.forEach(card => {
+        //     console.log(card)
+        // });
+
+        const cards = this.state.cards.map(card => {
+            return(
+                <Card 
+                key={uuid()}
+                cardUrl={card}
+                />
+            )
+        });
+    
         return (
             <div>
                 <button onClick={this.handleClick}>Gimme a card!</button>
+                {cards}
             </div>
         )
     }
