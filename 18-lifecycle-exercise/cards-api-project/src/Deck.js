d3b92e8bf0ec2a90aadb6cb03233bccab37db928
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-01 09:08:07 +0100

    Changed API URL to a const

--- 18-lifecycle-exercise/cards-api-project/src/Deck.js
+++ 18-lifecycle-exercise/cards-api-project/src/Deck.js
@@ -2,35 +2,53 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Card from './Card';
 
+const API_URL = "https://deckofcardsapi.com/api/deck/new/shuffle";
+
 class Deck extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { deckID: '', cards: ''};
+        this.state = { deckID: '', cards: '' };
 
+        this.handleClick = this.handleClick.bind(this);
     }
 
     async componentDidMount() {
-       // Get deck ID
-        axios.get('https://deckofcardsapi.com/api/deck/new/shuffle').then(response => {
+        // Get deck ID
+        axios.get(API_URL).then(response => {
             this.setState({ deckID: response.data.deck_id });
+
         });
 
-    //     const id = this.state.deckID;
-    //     axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/`).then(response => {
-    //         // this.setState({ deckID: response.data.deck_id });
-    //         console.log(response.data)
-    //     });
-    // }
+
+        // const id = this.state.deckID;
+        //     axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/`).then(response => {
+        //         // this.setState({ deckID: response.data.deck_id });
+        //         console.log(response.data)
+        //     });
     }
 
 
 
+
+
+    makeNewCard() {
+        return (
+            <Card dID={this.state.deckID} />
+        )
+
+    }
+
+    handleClick(evt) {
+        console.log('I was clicked')
+
+    }
+
+
     render() {
-        return(
+        return (
             <div>
-                <h1>I am the deck</h1>
-                <Card />
+                <button onClick={this.handleClick}>Gimme a card!</button>
             </div>
         )
     }
