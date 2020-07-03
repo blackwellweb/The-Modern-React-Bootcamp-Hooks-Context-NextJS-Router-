7b1f60d1226b330869c3f9ba65940241d27f7b4a
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-03 07:56:58 +0100

    Randomly Transforming Cards

--- 18-lifecycle-exercise/cards-api-project-solution/src/Deck.js
+++ 18-lifecycle-exercise/cards-api-project-solution/src/Deck.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import axios from 'axios';
+import './Deck.css';
+
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck/";
 
 class Deck extends Component {
@@ -57,7 +59,7 @@ class Deck extends Component {
             <div>
                 <h1>Card dealer</h1>
                 <button onClick={this.getCard}>Get Card!</button>
-                {cards}
+                <div className="Deck-cardarea">{cards}</div>
             </div>
         );
     }
