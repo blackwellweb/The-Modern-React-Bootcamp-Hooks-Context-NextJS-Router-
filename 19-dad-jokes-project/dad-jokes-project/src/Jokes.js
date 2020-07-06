f2afbbfb37654d6523a54cc355654f8b701d9e66
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-06 07:49:56 +0100

    Project setup

---
+++ 19-dad-jokes-project/dad-jokes-project/src/Jokes.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Jokes extends Component {
+    render(){
+        return(
+            <div class="Jokes">
+                <h1>I am the Jokes</h1>
+            </div>
+        )
+    }
+}
+
+
+
+export default Jokes;
