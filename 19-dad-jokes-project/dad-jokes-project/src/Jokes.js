bf0ff8ef2c13cc2edc40e3de9ab954430293325c
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-06 09:20:52 +0100

    Can now get Jokes from the API

--- 19-dad-jokes-project/dad-jokes-project/src/Jokes.js
+++ 19-dad-jokes-project/dad-jokes-project/src/Jokes.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const API_BASE_URL = "https://icanhazdadjoke.com/";
 
 class Jokes extends Component {
-    render(){
-        return(
-            <div class="Jokes">
+
+    constructor(props) {
+        super(props);
+
+        this.state = { jokes: '' }
+    }
+
+
+    async componentDidMount() {
+
+        let joke = await axios.get(API_BASE_URL, {
+            headers: { Accept: 'application/json' }
+        });
+    
+
+        const data = joke.data;
+
+        this.setState(curSate => ({
+            jokes: [
+                ...curSate.jokes,
+                {
+                    id: data.id,
+                    image: data.joke,
+                    score: ''
+                }
+            ]
+        }));
+        
+
+    }
+
+
+    render() {
+        return (
+            <div className="Jokes">
                 <h1>I am the Jokes</h1>
             </div>
         )
