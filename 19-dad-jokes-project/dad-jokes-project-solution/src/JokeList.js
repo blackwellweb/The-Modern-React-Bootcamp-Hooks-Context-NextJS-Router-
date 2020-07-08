321a97d12c164c6e8306a0f1b53278f38e13dba2
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-08 08:26:11 +0100

    Styling the Joke Component

--- 19-dad-jokes-project/dad-jokes-project-solution/src/JokeList.js
+++ 19-dad-jokes-project/dad-jokes-project-solution/src/JokeList.js
@@ -55,7 +55,7 @@ class JokeList extends Component {
                     <h1 className="JokeList-title">
                         <span>Dad</span> Jokes
                     </h1>
-                    <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+                    <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="image of a laughing emoji"/>
                     <button className="JokeList-getmore">New Joke</button>
                 </div>
                 <div className="JokeList-jokes">
