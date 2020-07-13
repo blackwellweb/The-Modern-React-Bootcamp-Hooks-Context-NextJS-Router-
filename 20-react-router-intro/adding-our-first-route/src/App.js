77b943778f79e957d41b3318f3c413242b4904ca
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-13 08:10:16 +0100

    Intro to the Link Component

--- 20-react-router-intro/adding-our-first-route/src/App.js
+++ 20-react-router-intro/adding-our-first-route/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import About from './About';
 import Contact from './Contact';
 import Dog from './Dog';
@@ -12,9 +12,20 @@ const Hater = () => <h1>I absolutely hate dogs!</h1>
 function App() {
   return (
     <div className="App">
+      <nav className="App-nav">
+        <Link to="/">About</Link>
+        <Link to="/dog">Dog</Link>
+        <Link to="/contact">Contact</Link>
+
+
+        {/* We don’t use the ‘a’ tag because we it will send a GET request to the server re-loading the page so un-link Link with will load in the Data client side  */}
+        {/* <a href='/'>About</a>
+        <a href='/dog'>Dog</a>
+        <a href='/contact'>Contact</a> */}
+
+      </nav>
       <Switch>
         <Route exact path="/" component={About} />
-        <Route exact path="/about" component={About} />
         <Route exact path="/dog" component={Dog} />
         <Route exact path="/dog/hater" component={Hater} />
         <Route exact path="/contact" component={Contact} />
