b0df24867ad9afafd46bd7db90710bee49dca58f
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-17 07:57:27 +0100

    Adding a 404 Not Found Route

--- 22-react-router-patterns/working-with-url-params/src/App.js
+++ 22-react-router-patterns/working-with-url-params/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Food from './Food';
 import Meal from './Meal';
 import './App.css';
@@ -8,16 +8,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* {...routeProps} will extract the items match, location and history and pass them down as three separate props to our Food component  */}
-      <Route exact path="/food/:name" render={(routeProps) => <Food {...routeProps} />} /> 
 
-      {/* this will do the same as above but you will not be able to pass in additional props for example component={Food, authenticated="true"} will NOT work*/}
-      {/* <Route exact path="/food/:name" component={Food} />  */}
+      <Switch>
+
+        <Route exact path="/" render={() => <h1>Home Page</h1>}/>
 
 
-      {/* Multiple Route Params */}
-      <Route exact path='/food/:foodName/drink/:drinkName' render={(routeProps) => <Meal {...routeProps} />}/>
+        {/* {...routeProps} will extract the items match, location and history and pass them down as three separate props to our Food component  */}
+        <Route exact path="/food/:name" render={(routeProps) => <Food {...routeProps} />} />
 
+        {/* this will do the same as above but you will not be able to pass in additional props for example component={Food, authenticated="true"} will NOT work*/}
+        {/* <Route exact path="/food/:name" component={Food} />  */}
+
+
+        {/* Multiple Route Params */}
+        <Route exact path='/food/:foodName/drink/:drinkName' render={(routeProps) => <Meal {...routeProps} />} />
+
+        {/* 404 Page not Found */}
+        <Route render={() => <h1>Error not found!!</h1>} />
+
+
+      </Switch>
 
     </div>
   );
