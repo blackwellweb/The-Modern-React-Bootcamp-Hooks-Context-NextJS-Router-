ade0fccf6fc41f9d8af2f1397c3c74a326a445aa
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-22 08:29:36 +0100

    Added redirect to dogs route

--- 23-router-patterns-dog-app/router-dogs-app/src/App.js
+++ 23-router-patterns-dog-app/router-dogs-app/src/App.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import NavBAr from './Navbar';
+import Dogs from './Dogs';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends Component {
+
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: 'imgs/whiskey.jpg',
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: 'imgs/hazel.jpg',
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: 'imgs/tubby.jpg',
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  }
+
+  render() {
+
+
+
+
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/dogs" /> 
+          </Route>
+          <Route exact path="/dogs" render={(routeProps) => <Dogs {...routeProps} data={this.props.dogs}/>} />
+        </Switch>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ 23-router-patterns-dog-app/router-dogs-app/src/Dogs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Dogs extends Component {
+    render(){
+        return(
+            <div className="Dogs">
+                <h1>I am dogs</h1>
+            </div>
+        )
+    }
+}
+
+export default Dogs;
