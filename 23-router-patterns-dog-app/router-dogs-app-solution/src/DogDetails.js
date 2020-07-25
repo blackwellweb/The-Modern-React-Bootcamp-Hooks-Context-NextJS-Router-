7892cf090978a1e442c81d0cc5d1b85d0204c6bc
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-25 09:12:09 +0100

    Adding the DogDetails Component (100%)

--- 23-router-patterns-dog-app/router-dogs-app-solution/src/DogDetails.js
+++ 23-router-patterns-dog-app/router-dogs-app-solution/src/DogDetails.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './DogDetails.css';
 
 // rcc
 
@@ -6,9 +8,26 @@ class DogDetails extends Component {
     render() {
         let { dog } = this.props;
         return (
-            <div className="DogDetails row">
-                <div className="col-11 col-5-lg">
-                    <h1>{this.props.dog.name}</h1>
+            <div className="container">
+                <div className="DogDetails row justify-content-center mt-5">
+                    <div className="col-11 col-lg-5">
+                        <div className="DogDetails-card card">
+                            <img className="card-img-top" src={dog.src} alt={dog.name} />
+                            <div className="card-body">
+                                <h2 className="card-title">{dog.name}</h2>
+                                <h4 className="card-subtitle text-muted">{dog.age} years old</h4>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                {/* You should use something like uuid not i (index) for the key */}
+                                {dog.facts.map((fact, i) => (
+                                    <li className="list-group-item" key={i}>{fact}</li>
+                                ))}
+                            </ul>
+                            <div className="card-body">
+                                <Link to="/dogs" className="btn btn-info">Go Back</Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
