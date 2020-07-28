2e0219c6cb5237ab5ea457edaf649b2a83e7c706
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-28 07:47:05 +0100

    Adding Color Slider

--- 24-the-massive-color-project/colors-app/src/Palette.js
+++ 24-the-massive-color-project/colors-app/src/Palette.js
@@ -6,14 +6,36 @@ import 'rc-slider/assets/index.css';
 
 
 class Palette extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { level: 500 };
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+
+    changeLevel(level) {
+        this.setState({ level });
+    }
+
+
+
     render() {
 
-        const colorBoxes = this.props.palette.colors[500].map(color => 
+        const { colors } = this.props.palette;
+        const { level } = this.state;
+        const colorBoxes = colors[this.state.level].map(color =>
             <ColorBox background={color.hex} name={color.name} />
         );
 
         return (
             <div className="Palette">
+                <Slider
+                    defaultValue={level}
+                    min={100}
+                    max={900}
+                    step={100}
+                    onAfterChange={this.changeLevel}
+                />
                 {/* Navbar goes here */}
                 <div className="Palette-colors">{colorBoxes}</div>
                 {/* footer eventually */}
