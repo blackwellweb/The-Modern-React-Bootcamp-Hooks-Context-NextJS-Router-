632a2b4d80e9cd5dcbc0c00f0bd49c2be58d4596
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-29 08:37:39 +0100

    ntro to Material UI & Adding Select

--- 24-the-massive-color-project/colors-app/src/Narbar.js
+++ 24-the-massive-color-project/colors-app/src/Narbar.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
+
 class Narbar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { format: 'hex' };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(evt) {
+        this.setState({ format: evt.target.value });
+        this.props.handleChange(evt.target.value );
+    }
+
+
     render() {
-        const { level, changeLevel } = this.props;
+        const { level, changeLevel} = this.props;
+        const { format } = this.state;
         return (
             <header className="Navbar">
                 <div className="logo">
@@ -22,6 +39,13 @@ class Narbar extends Component {
                             onAfterChange={changeLevel}
                         />
                     </div>
+                </div>
+                <div className="select-container">
+                    <Select value={format} onChange={this.handleChange}>
+                        <MenuItem value='hex'>Hex - #ffffff</MenuItem>
+                        <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
+                        <MenuItem value='rgba'>RGBA - rgb(255,255,255, 1.0)</MenuItem>
+                    </Select>
                 </div>
             </header>
         )
