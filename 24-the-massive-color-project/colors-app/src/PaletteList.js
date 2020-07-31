1a12a6f8f001be9e118830ec339e1c8c084c17a6
Paul Blackwell <blackwellpaul34@gmail.com>
2020-07-31 07:52:02 +0100

    Styling PaletteList with JSS (100%)

--- 24-the-massive-color-project/colors-app/src/PaletteList.js
+++ 24-the-massive-color-project/colors-app/src/PaletteList.js
@@ -5,13 +5,31 @@ import { withStyles } from "@material-ui/styles";
 
 const styles = {
     root: {
-        backgroundColor: "blue"
+        backgroundColor: "blue",
+        height: "100%",
+        display: "flex",
+        alignItem: "flex-start",
+        justifyContent: "center"
     },
     container: {
-
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap"
     },
     nav: {
-        
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        color: "white"
+    },
+    palettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 30%)",
+        gridGap: "5%"
     }
 }
 
