903a20a6a008e549d087a1748824116ee4cbb0f6
Paul Blackwell <blackwellpaul34@gmail.com>
2020-08-09 17:05:46 +0100

    Moving JSS Styles Out

--- 24-the-massive-color-project/colors-app/src/ColorPickerForm.js
+++ 24-the-massive-color-project/colors-app/src/ColorPickerForm.js
@@ -3,26 +3,8 @@ import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button"
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { ChromePicker } from 'react-color';
+import styles from './styles/ColorPickerForm';
 
-const styles = {
-    ColorPickerForm: {
-        width: "100%"
-    },
-    picker: {
-        width: "100% !important",
-        marginTop: "2rem"
-    },
-    addColor: {
-        width: "100%",
-        padding: "1rem",
-        marginTop: "1rem",
-        fontSize: "2rem"
-    },
-    colorNameInput: {
-        width: "100%",
-        height: "70px",
-    }
-}
 
 class ColorPickerForm extends Component {
     constructor(props) {
