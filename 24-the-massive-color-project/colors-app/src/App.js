8f16dd617acc9bfdaa07802bff51f439253e47bc
Paul Blackwell <blackwellpaul34@gmail.com>
2020-08-10 07:55:29 +0100

    Saving to LocalStorage

--- 24-the-massive-color-project/colors-app/src/App.js
+++ 24-the-massive-color-project/colors-app/src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = { palettes: savedPalettes || seedColors};
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
   }
@@ -26,7 +27,12 @@ class App extends Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState({ palettes: [...this.state.palettes, newPalette] }, this.syncLocalStorage);
+  }
+
+  syncLocalStorage() {
+    // Save palettes to local storage
+    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes))
   }
 
 
