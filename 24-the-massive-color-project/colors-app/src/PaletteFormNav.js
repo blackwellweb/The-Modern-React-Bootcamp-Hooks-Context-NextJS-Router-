71cc645e2ec31fe837483aeca5d1b0c2fc65c4b3
Paul Blackwell <blackwellpaul34@gmail.com>
2020-08-10 07:44:25 +0100

    Tweak Form Styles

--- 24-the-massive-color-project/colors-app/src/PaletteFormNav.js
+++ 24-the-massive-color-project/colors-app/src/PaletteFormNav.js
@@ -8,7 +8,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 import Button from "@material-ui/core/Button"
 import styles from './styles/PaletteFormNavStyles';
 
@@ -35,7 +35,7 @@ class PaletteFormNav extends Component {
     }
 
     hideForm() {
-        this.setState({formShowing: false})
+        this.setState({ formShowing: false })
     }
 
     render() {
@@ -55,9 +55,9 @@ class PaletteFormNav extends Component {
                             color='inherit'
                             aria-label='Open drawer'
                             onClick={this.props.handleDrawerOpen}
-                            className={classNames(classes.menuButton, open && classes.hide)}
+                            className={classNames(classes.menuButton, { [classes.hide]: open })}
                         >
-                            <MenuIcon />
+                            <AddToPhotosIcon />
                         </IconButton>
                         <Typography variant='h6' color='inherit' noWrap>
                             Create a Palette
@@ -72,7 +72,7 @@ class PaletteFormNav extends Component {
                         </Button>
                     </div>
                 </AppBar>
-                {this.state.formShowing && <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} hideForm={this.hideForm}/>}
+                {this.state.formShowing && <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} hideForm={this.hideForm} />}
             </div>
         )
     }
