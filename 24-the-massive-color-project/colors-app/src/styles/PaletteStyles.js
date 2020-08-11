03f2cc5619ec2b251666d8d47f99a35faaaef111
Paul Blackwell <blackwellpaul34@gmail.com>
2020-08-11 08:33:09 +0100

    Make Color Box Responsive

--- 24-the-massive-color-project/colors-app/src/styles/PaletteStyles.js
+++ 24-the-massive-color-project/colors-app/src/styles/PaletteStyles.js
@@ -1,3 +1,6 @@
+import sizes from './sizes';
+
+
 export default {
     Palette: {
         height: "100vh",
@@ -16,7 +19,7 @@ export default {
         position: "relative",
         cursor: "pointer",
         marginBottom: "-4px",
-        backgroundColor:"#000000",
+        backgroundColor: "#000000",
         "& a": {
             width: "100px",
             height: "30px",
@@ -28,13 +31,25 @@ export default {
             marginTop: "-15px",
             textAlign: "center",
             outline: "none",
-            background:  "rgba(255,255,255, 0.3)",
+            background: "rgba(255,255,255, 0.3)",
             fontSize: "1rem",
             lineHeight: "30px",
             color: "#ffffff",
             textTransform: "uppercase",
             border: "none",
             textDecoration: "none",
+        },
+        [sizes.down("lg")]: {
+            width: "25%",
+            height: "33.3333%"
+        },
+        [sizes.down("md")]: {
+            width: "50%",
+            height: "20%"
+        },
+        [sizes.down("xs")]: {
+            width: "100%",
+            height: "10%"
         }
-    }
+    },
 }
