297d19babdd6e8bb01a414158fb32bfd1efb9a53
Paul Blackwell <blackwellpaul34@gmail.com>
2020-08-14 08:09:49 +0100

    Cleaning Things Up

--- 24-the-massive-color-project/colors-app/src/ColorBox.js
+++ 24-the-massive-color-project/colors-app/src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
 import styles from './styles/ColorBoxStyles';
+import classNames from 'classnames';
 import { withStyles } from "@material-ui/styles";
 
 class ColorBox extends Component {
@@ -25,8 +26,8 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div className={classes.ColorBox} style={{ background }}>
-                    <div className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} style={{ background }} />
-                    <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
+                    <div className={classNames(classes.copyOverlay, {[classes.showOverlay]: copied})} style={{ background }} />
+                    <div className={classNames(classes.copyMessage, {[classes.showMessage]: copied})}>
                         <h1>copied!</h1>
                         <p className={classes.copyText}>{background}</p>
                     </div>
