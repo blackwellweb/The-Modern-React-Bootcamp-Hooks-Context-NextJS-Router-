500051254c30f87544a55eb2574228dee0a5cc17
Paul Blackwell <blackwellpaul34@gmail.com>
2020-08-18 08:40:38 +0100

    Editing w/ Hooks (10%)

--- 26-hooks-app/react-hooks-project/src/Todo.js
+++ 26-hooks-app/react-hooks-project/src/Todo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import useToggleState from './hooks/useToggleState';
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -6,8 +7,10 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import useToggle from './hooks/useToggleState';
 
 export default function Todo({ id, task, completed, removeTodo, toggleTodo}) {
+    const [isEditing, toggle] = useToggleState(false);
     return (
         <ListItem>
             <Checkbox tabIndex={-1} checked={completed} onClick={() => toggleTodo(id)}/>
