844bbacd33e2d855d11da42471ab1bae3b6ddda5
Paul Blackwell <blackwellpaul34@gmail.com>
2020-08-25 08:01:32 +0100

    Consuming 2 Contexts: Enter the Higher Order Component (100%)

--- 27-introducing-the-context-api/context-app/src/contexts/LanguageContext.js
+++ 27-introducing-the-context-api/context-app/src/contexts/LanguageContext.js
@@ -9,11 +9,17 @@ export class LanguageProvider extends Component {
         this.changeLanguage = this.changeLanguage.bind(this);
     }
 
-    changeLanguage(e){
-        this.setState({language: e.target.value});
+    changeLanguage(e) {
+        this.setState({ language: e.target.value });
     }
 
-    render(){
-        return <LanguageContext.Provider value={{...this.state, changeLanguage: this.changeLanguage}}>{this.props.children}</LanguageContext.Provider>
+    render() {
+        return <LanguageContext.Provider value={{ ...this.state, changeLanguage: this.changeLanguage }}>{this.props.children}</LanguageContext.Provider>
     }
 }
+
+export const withLanguageContext = Component => props => (
+    <LanguageContext.Consumer>
+        {value => <Component languageContext={value} {...props} />}
+    </LanguageContext.Consumer>
+);
