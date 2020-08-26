eeddfc0596687cd2447f422ff91ba70f955b6997
Paul Blackwell <blackwellpaul34@gmail.com>
2020-08-26 07:54:16 +0100

    "Hookify-ing" the Rest of the App

--- 28-using-context-with-hooks/context-app-with-hooks/src/PageContent.js
+++ 28-using-context-with-hooks/context-app-with-hooks/src/PageContent.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
 
-export default class PageContent extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const { isDarkMode } = this.context;
+export default function PageContent(props)  {
+   
+   
+        const { isDarkMode } = useContext(ThemeContext);
         const styles = {
             backgroundColor: isDarkMode ? "#000000" : "#ffffff",
             height: "100vh",
@@ -12,7 +12,6 @@ export default class PageContent extends Component {
         }
 
         return (
-            <div style={styles}>{this.props.children}</div>
-        )
-    }
+            <div style={styles}>{props.children}</div>
+        );
 }
