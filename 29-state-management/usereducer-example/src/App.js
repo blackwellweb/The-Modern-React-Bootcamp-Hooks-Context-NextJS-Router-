fda1e59570e333e0cd8ab94aa6fbf8a2baff4111
Paul Blackwell <blackwellpaul34@gmail.com>
2020-08-28 07:59:39 +0100

    First useReducer Example (100%)

--- 29-state-management/usereducer-example/src/App.js
+++ 29-state-management/usereducer-example/src/App.js
@@ -2,21 +2,38 @@ import React, { useReducer } from 'react';
 import './App.css';
 
 
-function reducer(state, action) {
-  if (action.type === "INCREMENT") return { count: state.count + action.amount };
-  if (action.type === "DECREMENT") return { count: state.count - action.amount };
+function countReducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + action.amount };
+    case "DECREMENT":
+      return { count: state.count - action.amount };
+    case "RESET":
+      return { count: 0 };
+  }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 })
+  const [state, dispatch] = countReducer(reducer, { count: 0 })
   return (
     <div className="App">
       <h1>{state.count}</h1>
       <button onClick={() => dispatch({ type: "INCREMENT", amount: 1 })}>Add 1</button>
       <button onClick={() => dispatch({ type: "INCREMENT", amount: 5 })}>Add 5</button>
       <button onClick={() => dispatch({ type: "DECREMENT", amount: 1 })}>Subtract 1</button>
+      <button onClick={() => dispatch({ type: "RESET"})}>Reset</button>
     </div>
   );
 }
 
 export default App;
+
+
+// (accumulatedValue, nextItem) => nextAccumulatedValue
+// takes two values and "reduces" them down to one value
+
+// In our case, the two values provided to a reducer are:
+// - The current state
+// - An action that (may) update the state
+
+// (state, action) => newState 
