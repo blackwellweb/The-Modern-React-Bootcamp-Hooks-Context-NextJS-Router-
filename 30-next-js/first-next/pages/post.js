2f7027bf86afb0ba4ded78e247cc72316c44abcb
Paul Blackwell <blackwellpaul34@gmail.com>
2020-09-19 10:33:32 +0100

    withRouter Higher Order Component

--- 30-next-js/first-next/pages/post.js
+++ 30-next-js/first-next/pages/post.js
@@ -1,3 +1,23 @@
+
+
+
+/**
+ * If you don't want to use  getServerSideProps or 
+ * getStaticProps you can use withRouter
+ * THIS WILL NOT BE RAIN SERVER SIDE!! (use getServerSideProps  for that)
+ */
+
+// import { withRouter } from 'next/router';
+
+// const Post = withRouter((props) => {
+//     const { id } = props.router.query;
+//     return (
+//         <h1>Post Page # {id}</h1>
+//     );
+// });
+
+
+
 const Post = (props) => {
     const {id} = props.query;
     return (
@@ -10,6 +30,6 @@ const Post = (props) => {
 export async function getServerSideProps({query}) {
     return { props: { query }};
 }
-  
+
 
 export default Post;
